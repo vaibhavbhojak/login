7a48bb4b14d30ec8776cde589c62bcf016798b8d
vaibhavbhojak <77951894+vaibhavbhojak@users.noreply.github.com>
2021-01-28 10:46:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ readfile('login.php');
 readfile('registration.php');
 readfile('validation.php');
 readfile('home.php');
-readfile('db.opt');
-readfile('signin.frm');
-readfile('signin.ibd');
 
 
 ?>
